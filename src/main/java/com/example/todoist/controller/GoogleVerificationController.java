599293a79c672c6838840e97ccb2eb2ff0cdcfbd
Niti Sethi <55200512+thebuttercup@users.noreply.github.com>
2020-01-18 00:20:10 +0530

    Create GoogleVerificationController.java
    Adding Google verification controller

---
+++ src/main/java/com/example/todoist/controller/GoogleVerificationController.java
@@ -0,0 +1,29 @@
+package com.example.todoist.controller;
+
+import com.example.todoist.requestBean.TaskRequest;
+import com.example.todoist.responseBean.ActiveTaskResponse;
+import com.example.todoist.responseBean.CreateTaskResponse;
+import com.example.todoist.service.TaskService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@Slf4j
+@RestController
+@RequestMapping("/")
+public class GoogleVerificationController {
+
+    @GetMapping("/googlec4994c3d3ef026f1")
+    public ResponseEntity<String> getActiveTaskList() {
+        return new ResponseEntity<String>("google-site-verification: googlec4994c3d3ef026f1.html", HttpStatus.OK);
+    }
+    
+    @GetMapping("/googlec4994c3d3ef026f1.html")
+    public ResponseEntity<String> getActiveTaskList() {
+        return new ResponseEntity<String>("google-site-verification: googlec4994c3d3ef026f1.html", HttpStatus.OK);
+    }
+}
